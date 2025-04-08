bc43c0ec539d76d3a73adaa74553b56c26fa6341
Huzaifa <shuzafa222@gmail.com>
2025-04-08 20:38:34 +0500

    app.listen() removed

--- api/index.js
+++ api/index.js
@@ -8,14 +8,20 @@ const todoRoute = require("../routes/Todo.routes");
 
 const app = express();
 
-// middleware
+// Middleware
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cors());
 
-// routes
+// Routes
 app.use("/api/todos", todoRoute);
 
+// Validate MONGODB_URL exists
+if (!process.env.MONGODB_URL) {
+  throw new Error("MONGODB_URL is not set in the environment variables.");
+}
+
+// Connect to MongoDB
 mongoose
   .connect(process.env.MONGODB_URL)
   .then(() => {
@@ -25,4 +31,5 @@ mongoose
     console.error("❌ Error connecting to database:", error);
   });
 
+// Export the serverless handler
 module.exports.handler = serverless(app);
